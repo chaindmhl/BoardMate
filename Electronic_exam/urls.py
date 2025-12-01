03ca02d6e8bb7a7654817f1e95d7e2515e2108be
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2025-12-01 13:15:23 +0800

    Update urls.py

--- Electronic_exam/urls.py
+++ Electronic_exam/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]# still available
-]
+
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
