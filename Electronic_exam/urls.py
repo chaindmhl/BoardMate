020ba0104d6a433e73e8df8e04c1318981dd02ed
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2025-12-01 13:25:14 +0800

    Update urls.py

--- Electronic_exam/urls.py
+++ Electronic_exam/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     
     path('signup/', views.signup, name='signup'),
     path('home/', views.home, name='home'),  
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]# still available
 
 # Serve media files in development
