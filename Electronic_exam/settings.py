d15849b58ae401e076a077e462a1f400ffb85e25
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2025-12-03 15:03:02 +0800

    Update settings.py

--- Electronic_exam/settings.py
+++ Electronic_exam/settings.py
@@ -15,7 +15,7 @@ LOGIN_REDIRECT_URL = '/'         # URL to redirect after login
 LOGOUT_REDIRECT_URL = '/'        # URL after logout
 
 # URL of your Colab Flask server (ngrok public URL)
-COLAB_URL = "https://kasi-releasible-conscionably.ngrok-free.dev"
+COLAB_URL = "https://kasi-releasible-conscionably.ngrok-free.dev/"
 
 
 # Quick-start development settings - unsuitable for production
